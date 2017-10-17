e08a93571e2d0d28053b7e6569b6f8a8ee008089
Nanne Baars <nanne.baars@owasp.org>
2017-10-18 01:17:42 +0200

    working Docker setup

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-set -ex
-
-cd ..
-mvn clean install
-
-cd webgoat-server
-docker
--- webgoat-server/src/main/docker/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-java -Xmx256m -Xss256k -Djava.security.egd=file:/dev/./urandom -jar /home/webgoat/webgoat.jar &
-#echo "Waiting for WebGoat to start..."
-#sleep 20
-#java -Xmx128m -Xss256k -Djava.security.egd=file:/dev/./urandom -jar /home/webgoat/webwolf.jar
