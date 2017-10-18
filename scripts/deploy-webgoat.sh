95a4456f92f9b9b5f9af39c28e437888b66f0935
Nanne Baars <nanne.baars@owasp.org>
2017-10-18 06:39:43 +0200

    Removed ActiveMQ

---
+++ scripts/deploy-webgoat.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+export REPO=webgoat/webgoat-travis
+
+if [ -z "${TRAVIS_TAG}" ]; then
+  echo "Tag build";
+else
+  echo "Build ${BRANCH}";
+fi
+
+#docker login -u $DOCKER_USER -p $DOCKER_PASS
+#- export REPO=webgoat/webgoat-travis
+#- export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+#- docker build -f Dockerfile -t $REPO .
+#- docker push $REPO
